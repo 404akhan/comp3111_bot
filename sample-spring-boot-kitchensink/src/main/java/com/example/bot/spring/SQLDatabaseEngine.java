36477765fc5efa159506b648a44ba3384b467dbf
404akhan <404akhan@gmail.com>
2017-10-29 17:25:07 +0800

    def

--- sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/SQLDatabaseEngine.java
+++ sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/SQLDatabaseEngine.java
@@ -5,30 +5,58 @@ import javax.annotation.PostConstruct;
 import javax.sql.DataSource;
 import java.sql.*;
 import java.net.URISyntaxException;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.URI;
+
+// @Slf4j
+// public class SQLDatabaseEngine {
+// 	String search(String text) throws Exception {
+// 		String tvoya_mamka = "Fuck_yeah";
+// 		return tvoya_mamka;
+// 	}
+// }
+
 
 @Slf4j
 public class SQLDatabaseEngine {
 	String search(String text) throws Exception {
-		String tvoya_mamka = "Fuck_yeah";
-		return tvoya_mamka;
+		String result = null;
+		
+		Connection connection = getConnection();
+		PreparedStatement stmt = connection.prepareStatement("SELECT * FROM keyresp2");
+		ResultSet rs = stmt.executeQuery();
+
+		while(rs.next()) {
+			if (text.toLowerCase().contains(rs.getString(2).toLowerCase())) {
+				result = rs.getString(3);
+				return result;
+			}
+		}
+		rs.close();
+		stmt.close();
+		connection.close();
+
+		return "NOT FOUND";
 	}
 	
 	
-	// private Connection getConnection() throws URISyntaxException, SQLException {
-	// 	Connection connection;
-	// 	URI dbUri = new URI(System.getenv("DATABASE_URL"));
+	private Connection getConnection() throws URISyntaxException, SQLException {
+		Connection connection;
+		URI dbUri = new URI(System.getenv("DATABASE_URL"));
 
-	// 	String username = dbUri.getUserInfo().split(":")[0];
-	// 	String password = dbUri.getUserInfo().split(":")[1];
-	// 	String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath() +  "?ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory";
+		String username = dbUri.getUserInfo().split(":")[0];
+		String password = dbUri.getUserInfo().split(":")[1];
+		String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath() +  "?ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory";
 
-	// 	log.info("Username: {} Password: {}", username, password);
-	// 	log.info ("dbUrl: {}", dbUrl);
+		log.info("Username: {} Password: {}", username, password);
+		log.info ("dbUrl: {}", dbUrl);
 		
-	// 	connection = DriverManager.getConnection(dbUrl, username, password);
+		connection = DriverManager.getConnection(dbUrl, username, password);
 
-	// 	return connection;
-	// }
+		return connection;
+	}
 
 }
+
